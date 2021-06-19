342fcbf5a8a5ae02f6cc1008dc2b03e12c4381b8
Yeongrok <ygim.github@gmail.com>
2021-06-19 20:21:15 +0900

    minor fix (bed size)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1343,8 +1343,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS 176
-#define Y_MAX_POS 178
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 180
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
